45e9667dd23991c329c51f76591b1478aede5fe6
SEBASTIAN\Rickdcar <rickdcar@rickdcar.com>
2022-05-17 19:49:39 +0200

    Control de soporte de AR por parte del dispositivo

--- Assets/Scripts/MenuSUMAR.cs
+++ Assets/Scripts/MenuSUMAR.cs
@@ -2,10 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.XR.ARFoundation;
 
 public class MenuSUMAR : MonoBehaviour
 {
     [SerializeField] private Animator anim;
+    [SerializeField] private ARSession arSession;
+    [SerializeField] private TextMeshProUGUI errorMessage;
+    [SerializeField] private GameObject button;
+
+
+
+    IEnumerator Start()
+    {
+        errorMessage.text = "";
+        if ((ARSession.state == ARSessionState.None) ||
+            (ARSession.state == ARSessionState.CheckingAvailability))
+        {
+            yield return ARSession.CheckAvailability();
+        }
+
+        if (ARSession.state == ARSessionState.Unsupported)
+        {
+            errorMessage.text = "ERROR - El dispositivo no acepta AR";
+            button.SetActive(false);
+        }
+        else
+        {
+            // Start the AR session
+            arSession.enabled = true;
+            button.SetActive(true);
+        }
+    }
 
 
     public void StartGame()
